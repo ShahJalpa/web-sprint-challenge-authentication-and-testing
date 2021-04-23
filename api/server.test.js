f4b6da45448a494ed7aea396391cff461cdc1c0b
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-23 15:42:41 -0400

    insert and post request tests are passing

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,62 @@
+const request = require('supertest');
+const db = require('../data/dbConfig');
+const server = require('./server.js');
+
+const Users = require('./auth/user-model');
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async ()=>{
+  await db('users').truncate()
+})
+afterAll(async ()=>{
+  await db.destroy()
+})
+
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+test('process.env.DB_ENV must be "testing"', () => {
+  expect(process.env.NODE_ENV).toBe('testing')
+})
+
+describe('the API endpoints', () => {
+  let user;
+  beforeEach(async () => {
+    user = {
+      username: 'Captain Marvel',
+      password: 'foobar'
+    }
+  })
+  describe('user-model, insert', () => {
+    it('creates new user in the database', async() => {
+      await Users.insert(user);
+      expect(await db('users')).toHaveLength(1);
+      expect((await db('users'))[0]).toEqual({
+        id: 1,
+        username: 'Captain Marvel',
+        password: 'foobar'
+      })
+    })
+  })
+
+  describe('[POST] /api/auth/register', () => {
+    it('creates new user in the database', async() => {
+      await request(server).post('/api/auth/register').send(user);
+      expect(await db('users')).toHaveLength(1);
+    })
+    it('should return newly created user', async() => {
+      const res = await request(server)
+            .post('/api/auth/register')
+            .send(user);
+          expect(res.body.id).toEqual(1);
+          expect(res.body.username).toEqual('Captain Marvel');
+    })
+  })
+
+
 })
