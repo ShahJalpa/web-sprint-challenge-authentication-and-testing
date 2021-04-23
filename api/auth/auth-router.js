58686423126b604c8a3c93914d63f98124d40435
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-23 14:26:25 -0400

    registeration added in router

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,33 +8,22 @@ const { validUser, isAvailableUser } = require('../middleware/user-mdlwr');
 
 const Users = require('./user-model');
 
-router.post('/register', (req, res) => {
-  res.end('implement register, please!');
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+// <----------- REGISTER USER ---------->
+router.post('/register', validUser, isAvailableUser, (req, res, next) => {
+  const credentials = req.body;
 
-    1- In order to register a new account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel", // must not exist already in the `users` table
-        "password": "foobar"          // needs to be hashed before it's saved
-      }
+  var salt = bcryptjs.genSaltSync(8);
+  var hash = bcryptjs.hashSync(credentials.password, salt)
 
-    2- On SUCCESSFUL registration,
-      the response body should have `id`, `username` and `password`:
-      {
-        "id": 1,
-        "username": "Captain Marvel",
-        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
-      }
+  credentials.password = hash;
+  credentials.username = credentials.username.trim();
 
-    3- On FAILED registration due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED registration due to the `username` being taken,
-      the response body should include a string exactly as follows: "username taken".
-  */
+  Users.insert(credentials)
+      .then(user => {
+        res.status(200).json(user)
+      })
+      .catch(next)
+  
 });
 
 router.post('/login', (req, res) => {
@@ -65,3 +54,30 @@ router.post('/login', (req, res) => {
 });
 
 module.exports = router;
+
+
+/*  ------------------REGISTER------------
+    IMPLEMENT
+    You are welcome to build additional middlewares to help with the endpoint's functionality.
+    DO NOT EXCEED 2^8 ROUNDS OF HASHING!
+
+    1- In order to register a new account the client must provide `username` and `password`:
+      {
+        "username": "Captain Marvel", // must not exist already in the `users` table
+        "password": "foobar"          // needs to be hashed before it's saved
+      }
+
+    2- On SUCCESSFUL registration,
+      the response body should have `id`, `username` and `password`:
+      {
+        "id": 1,
+        "username": "Captain Marvel",
+        "password": "2a$08$jG.wIGR2S4hxuyWNcBf9MuoC4y0dNy7qC/LbmtuFBSdIhWks2LhpG"
+      }
+
+    3- On FAILED registration due to `username` or `password` missing from the request body,
+      the response body should include a string exactly as follows: "username and password required".
+
+    4- On FAILED registration due to the `username` being taken,
+      the response body should include a string exactly as follows: "username taken".
+  */
