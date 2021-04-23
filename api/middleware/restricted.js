5517e872cca8613b0b85b495ada9052bd7f4f5f7
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-23 14:53:43 -0400

    restriction added in mdlwr for token validation

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../../config/secret');
+
 module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+  const token = req.headers.authorization;
 
-    1- On valid token in the Authorization header, call next.
-
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
-
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  if(!token) {
+    res.status(401).json({ message: 'token required' })
+  } else {
+    jwt.verify(token, jwtSecret, (error, valid) => {
+      if(error){
+        res.status(401).json({ message: 'token invalid' });
+      } else {
+        next();
+      }
+    })
+  }
 };
+/*
+  IMPLEMENT
+
+  1- On valid token in the Authorization header, call next.
+
+  2- On missing token in the Authorization header,
+    the response body should include a string exactly as follows: "token required".
+
+  3- On invalid or expired token in the Authorization header,
+    the response body should include a string exactly as follows: "token invalid".
+*/
